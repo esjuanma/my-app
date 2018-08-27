9d8ffc4a0ac8a082a6fbb8db4dc1d4148c881d8d
juanmartin <juan.elfers@fizzmod.com>
2018-08-27 09:00:36 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { Provider } from 'react-redux';
+import Store from './store';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
